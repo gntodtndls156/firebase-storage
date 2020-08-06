53eeac68c8f9c04cdbfc102d076b5b41ef9cfe74
후생 <gntodtndls156@gmail.com>
2020-08-06 12:07:44 +0900

    Send Email

--- App.js
+++ App.js
@@ -4,11 +4,28 @@ import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 import Imageuploader from './Imageuploader';
 import ImageView from './Imageview';
+
+// 작업
+import * as MailComposer from 'expo-mail-composer'
+
+async function sendEmailAsync() {
+  let result = await MailComposer.composeAsync({
+    recipients: ['gntodtndls156@gmail.com'],
+    subject: "제목",
+    body: "사용하시는 기종이 무엇입니까? \n무슨 일입니까?"
+  })
+
+  Alert.alert(result.status);
+}
+
+// -------------
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Imageuploader />
       <ImageView />
+      <Button title="Send Email" onPress={sendEmailAsync} />
     </View>
   );
 }
